e59f495e541cddaec20289372651643b2ff87ffa
Riley Wong <rnwong@scu.edu>
2024-02-17 15:22:45 -0800

    Frontend Ligma Sigma

--- fistbump/src/views/MainView.vue
+++ fistbump/src/views/MainView.vue
@@ -1,11 +1,19 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+<script>
+export default {
+  name: "MainView",
+};
 </script>
 
 <template>
- <h1> Hello World</h1>
-</template>
+  <div style="display: grid; gridTemplateColumns: 2fr 5fr;">
+    <div style="grid-column: 1; background: red; height: 100vh">
+      Ligma
+    </div>
+    <div style="grid-column: 2; background: blue; height: 100vh; overflow-y: auto; overflow-x: hidden">
+      <p style="font-size: 500px">Sigma</p>
+    </div>
+  </div>
+  </template>
 
 <style>
 

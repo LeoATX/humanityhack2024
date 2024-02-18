9ffe05a53aa67d382ac239cf56c9927fd06807d7
Riley Wong <rnwong@scu.edu>
2024-02-17 16:23:57 -0800

    Create Navbar component

---
+++ fistbump/src/components/MainNavbar.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "MainNavbar",
+};
+
+</script>
+
+<template>
+  <nav style="position: fixed; width: 74.05%; background: gray; display: block">
+    <div style="float: left; display: flex">
+      <img src="https://images.heb.com/is/image/HEBGrocery/000374723-1" width="75px" height="75px">
+      <h1>asfa</h1>
+    </div>
+    <div style="float: right; margin-top: 25px; margin-right: 25px;">
+      <div style="border: 2px solid">
+        manage
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+
+.root {
+  --primary: #FFD464
+}
+
+</style>
--- fistbump/src/views/MainView.vue
+++ fistbump/src/views/MainView.vue
@@ -1,6 +1,11 @@
 <script>
+import MainNavbar from '@/components/MainNavbar.vue'
+
 export default {
   name: "MainView",
+  components: {
+    MainNavbar,
+  }
 };
 </script>
 
@@ -10,17 +15,8 @@ export default {
       Ligma
     </div>
     <div style="grid-column: 2; background: white; height: 100vh; overflow-y: auto; overflow-x: hidden">
-      <nav style="position: fixed; width: 74.05%; background: gray; display: block">
-        <div style="float: left; display: flex">
-          <img src="https://images.heb.com/is/image/HEBGrocery/000374723-1" width="75px" height="75px">
-          <h1>asfa</h1>
-        </div>
-        <div style="float: right; margin-top: 25px; margin-right: 25px;">
-          <div style="border: 2px solid">
-            manage
-          </div>
-        </div>
-      </nav>
+      <MainNavbar />
+
       <p style="font-size: 500px">Sigma</p>  
       <p style="font-size: 500px">Sigma</p>  
       <p style="font-size: 500px">Sigma</p>     

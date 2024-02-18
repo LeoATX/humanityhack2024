16501ca8817a7fb7616f6fe27858a71dcc4b4ddf
aceeedev <cyberandrew03@gmail.com>
2024-02-17 16:27:03 -0800

    Started click and drag

--- fistbump/src/main.js
+++ fistbump/src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueDragSelect from "@coleqiu/vue-drag-select";
 
 const app = createApp(App)
 
+app.use(VueDragSelect);
 app.use(router)
 
 app.mount('#app')
--- fistbump/src/views/MainView.vue
+++ fistbump/src/views/MainView.vue
@@ -1,18 +1,29 @@
 <script>
-import MainNavbar from '@/components/MainNavbar.vue'
+  import MainNavbar from '@/components/MainNavbar.vue'
+  import { ref } from 'vue';
 
-export default {
-  name: "MainView",
-  components: {
-    MainNavbar,
-  }
-};
+  const selection = ref([]);
+  const options = [...Array(95).keys()];
+
+  export default {
+    name: "MainView",
+    components: {
+      MainNavbar,
+    },
+    data: () =>  {
+      return {
+        "options": options
+      }
+    }
+  };
 </script>
 
 <template>
-  <div style="display: grid; gridTemplateColumns: 25% 75%;">
-    <div style="grid-column: 1; background: blue  ; height: 100vh">
-      Ligma
+  <div style="display: grid; gridTemplateColumns: 2fr 5fr;">
+    <div style="grid-column: 1; background: white; height: 100vh">
+      <drag-select v-model="selection">
+        <drag-select-option v-for="item in options" :value="item" :key="item">⠀</drag-select-option>
+      </drag-select>
     </div>
     <div style="grid-column: 2; background: white; height: 100vh; overflow-y: auto; overflow-x: hidden">
       <MainNavbar />
@@ -25,5 +36,15 @@ export default {
   </template>
 
 <style>
+  .drag-select-option {
+    width: 100px;
+    height: 5x;
+    color: #ffffff;
+    background: #E37E26;
+  }
 
+  .drag-select-option--selected {
+    color: #000000;
+    background: #5fdddc;
+  }
 </style>

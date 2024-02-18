f0349a0a9faaf6bb068df581430a55a4acce557e
Riley Wong <rnwong@scu.edu>
2024-02-17 16:17:36 -0800

    Better Navbar

--- fistbump/src/views/MainView.vue
+++ fistbump/src/views/MainView.vue
@@ -6,11 +6,21 @@ export default {
 
 <template>
   <div style="display: grid; gridTemplateColumns: 25% 75%;">
-    <div style="grid-column: 1; background: white; height: 100vh">
+    <div style="grid-column: 1; background: blue  ; height: 100vh">
       Ligma
     </div>
     <div style="grid-column: 2; background: white; height: 100vh; overflow-y: auto; overflow-x: hidden">
-      <nav style="position: fixed; width: 74.05%; background: white">asd</nav>
+      <nav style="position: fixed; width: 74.05%; background: gray; display: block">
+        <div style="float: left; display: flex">
+          <img src="https://images.heb.com/is/image/HEBGrocery/000374723-1" width="75px" height="75px">
+          <h1>asfa</h1>
+        </div>
+        <div style="float: right; margin-top: 25px; margin-right: 25px;">
+          <div style="border: 2px solid">
+            manage
+          </div>
+        </div>
+      </nav>
       <p style="font-size: 500px">Sigma</p>  
       <p style="font-size: 500px">Sigma</p>  
       <p style="font-size: 500px">Sigma</p>     
